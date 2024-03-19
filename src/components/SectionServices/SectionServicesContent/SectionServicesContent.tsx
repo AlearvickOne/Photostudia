de8299b289b77210e6538ad99823638686db4bce
AlearvickOne <Alexandr73.73.73.73@mail.ru>
2024-03-20 02:27:39 +0300

    servicesCard

--- src/components/SectionServices/SectionServicesContent/SectionServicesContent.tsx
+++ src/components/SectionServices/SectionServicesContent/SectionServicesContent.tsx
@@ -1,34 +1,35 @@
 import { useState } from "react";
 import classesCss from "./sectionServicesContent.module.scss";
 
-export default function SectionServicesContent(): JSX.Element {
-  const title = "Комерческие съемки";
+interface IContent {
+  contentTitle: string;
+  contentText: string;
+  contentPictures: string;
+}
 
+function TitleContent(title: string): JSX.Element {
+  return (
+    <span>
+      <h5 className={classesCss.contentTitle}>{title}</h5>
+    </span>
+  );
+}
+
+export default function SectionServicesContent({ contentTitle, contentText, contentPictures }: IContent): JSX.Element {
   const [isClick, setIsClick] = useState<boolean>(false);
-
-  const clickEvent = () => {
-    setIsClick(!isClick);
-  };
+  const titleElement = TitleContent(contentTitle);
+  const text = contentText;
 
   return (
-    <div className={classesCss.servicesContent} onClick={() => clickEvent()}>
+    <div className={classesCss.servicesContent} onClick={() => setIsClick(!isClick)}>
       {isClick ? (
         <div className={`${classesCss.activeAnim} ${classesCss.contentInformation}`}>
-          <h5 className={classesCss.contentTitle}>{title}</h5>
-          <div className={classesCss.contentText}>
-            <p>
-              Маркетплейсы, каталожные, рекламные) на выгодных <br /> условиях от 1000₽/час.
-            </p>
-            <p>
-              В неё входит: идеально белая и чистая циклорама <br />, профессиональное оборудование, фото/видеосъемка <br />
-              по запросу.
-            </p>
-            <p>Пробный час в подарок</p>
-          </div>
+          {titleElement}
+          <div className={classesCss.contentText} dangerouslySetInnerHTML={{ __html: text }} />
         </div>
       ) : (
-        <span className={`${classesCss.activeAnim} ${classesCss.contentPicture}`}>
-          <h5 className={classesCss.contentTitle}>{title}</h5>
+        <span className={`${classesCss.activeAnim} ${classesCss.contentPicture}`} style={{ backgroundImage: `url(${contentPictures})` }}>
+          {titleElement}
         </span>
       )}
     </div>
--- src/components/SectionServices/SectionsServices.tsx
+++ src/components/SectionServices/SectionsServices.tsx
@@ -1,18 +1,16 @@
 import SectionServicesContent from "./SectionServicesContent/SectionServicesContent";
 import SectionServicesTitle from "./SectionServicesTitle/SectionServicesTitle";
 import classesCss from "./sectionServices.module.scss";
+import { contents } from "./SectionServicesContent/contents";
 
 export default function SectionServices(): JSX.Element {
   return (
     <section className={classesCss.sectionServices}>
       <SectionServicesTitle />
       <div className={classesCss.sectionServicesContentGrid}>
-        <SectionServicesContent />
-        <SectionServicesContent />
-        <SectionServicesContent />
-        <SectionServicesContent />
-        <SectionServicesContent />
-        <SectionServicesContent />
+        {contents.map((el, i) => (
+          <SectionServicesContent key={i} contentTitle={el.contentTitle} contentText={el.contentText} contentPictures={el.photo} />
+        ))}
       </div>
     </section>
   );
