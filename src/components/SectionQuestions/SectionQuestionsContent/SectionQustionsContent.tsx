1eb2999ef480b99e0ab2e958aa402f141533ed58
AlearvickOne <alexandr73.73.73.73@mail.ru>
2024-03-27 16:57:44 +0300

    --

--- src/components/SectionQuestions/SectionQuestionsContent/SectionQustionsContent.tsx
+++ src/components/SectionQuestions/SectionQuestionsContent/SectionQustionsContent.tsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import classesCss from "./sectionQuestionContent.module.scss";
 
 export const SectionQustionsContent = () => {
+  const [isBtnClick, setIsBtnClick] = useState(false);
+  
   return (
     <ul className={classesCss.sectionQuestionContentList}>
       <li className={classesCss.sectionQuestionsContentFlex}>
-        <h5>01</h5>
-        <h6>В аквазале вода теплая?</h6>
-        <img src="#!" alt="img" />
+        <h5 className={classesCss.sectionQuestionContentNumber}>01</h5>
+        <div className={classesCss.sectionQuestionsContentHead}>
+          <h6 className={classesCss.sectionQuestionContentTitle}>В аквазале вода теплая?</h6>
+          
+        </div>
+        <span>
+                <button type="button" onClick={() => setIsBtnClick(!isBtnClick)}>
+                  <img src="src/assets/iconPlayVideo.png" alt="img" />
+                </button>
+        </span>
+
+        {isBtnClick && <p>Да, вода теплая, регулируется 37-40 градусов. Кроме этого <br /> установлены теплые полы для вашего комфорта. </p>}
+       
       </li>
     </ul>
   );
